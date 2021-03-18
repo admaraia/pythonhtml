7e902892e00dfa57e799005af6045f258519e2ed
admaraia <admaraia@gmail.com>
2021-03-18 18:31:10 +0000

    hello-world

--- microblog.py
+++ microblog.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask ("microblog")
-
-# operador do python que faz algo com a funcao logo abaixo
-@app.route("/")
-def index():
-    return "Olá Mundo!"
-
-app.run()
+from app import app
